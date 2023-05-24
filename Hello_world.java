937bf5d7b75263971392a151abce44ee80081d66
ArijitNaskar12 <arijitnaskar378@gmail.com>
2023-05-25 00:12:52 +0530

    Hello_world basics java

---
+++ Hello_world.java
@@ -0,0 +1,19 @@
+/**
+ * Hello_world
+//  */
+// jdk:java developement kit
+// jre:java runtime enviroment
+// jvm:java virtual machine
+// Variables
+class Hello_world{
+  //global variable
+public static void main(String args[])
+{
+    // boolean isTrue=false;// local variable;
+    double a=5.689;
+    double b=6.2548;
+    double c=(a/b);
+    System.out.println(c);
+}
+
+}
