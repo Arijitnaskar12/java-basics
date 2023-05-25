f8973a11af5a74be092793160d2cc79f24888d94
ArijitNaskar12 <arijitnaskar378@gmail.com>
2023-05-25 18:26:19 +0530

    operators in java

--- Hello_world.java
+++ Hello_world.java
@@ -6,14 +6,73 @@
 // jvm:java virtual machine
 // Variables
 class Hello_world{
+  int a=12;
   //global variable
 public static void main(String args[])
 {
     // boolean isTrue=false;// local variable;
-    double a=5.689;
-    double b=6.2548;
-    double c=(a/b);
-    System.out.println(c);
-}
+    // int a=10;
+    // System.out.println(a);
+    // unary operators
+    // 1)pre 
+    // 2)post
+    // increment or decrement
+    // // i)pre-increment(++variable_name(variable=variable+1))
+    // int a=10;
+    // System.out.println(++a);
+    // i)post-increment(variable_name++(variable=variable+1))
+    // int a=10;
 
+    // System.out.println(a++);
+    // System.out.println(a);
+    // i)pre-decrement(++variable_name(variable=variable-1))
+    // int a=10;
+    // System.out.println(--a);
+    // i)post-decrement(++variable_name(variable=variable-1))
+    // int a=10;
+    // System.out.println(a--);
+    // int i = 11;
+         
+    //     i = i++ + ++i;
+         
+    //     System.out.println(i);
+    // int a=11, b=22, c;
+         
+    //     c = a + b + a++ + b++ + ++a + ++b;
+         
+    //     System.out.println("a="+a);
+    //     System.out.println("b="+b);
+    //     System.out.println("c="+c);
+    // int i=0;
+         
+    //     i = i++ - --i + ++i - i--;
+         
+    //     System.out.println(i);
+    // int x = 1, y=10, z=100;
+   
+    // int i = --x + y++ - z-- - --z + ++y - --x + y-- - --x;
+     
+    // System.out.println("x="+x); //-2
+    // System.out.println("y="+y); //11
+    // System.out.println("z="+z);//98
+    // System.out.println("i="+i); //-161
+    // binary operators
+    // // 1.Arthmetic operator(+,-,*,/,%(modulo operator));
+    // int a=10;
+    // int b=23;
+    // System.out.println(b%a);
+//     // relational operators(>,<,<=,>=,==,!=)
+//     int a=10;
+//     int b=20;
+//     System.out.println(a==b);
+// }
+// logical operators(&&,||)
+// int a=10;
+// int b=10;
+// System.out.println(((a>b) || (a==b)));
+// assignment operators(+=,-=,*=,/=,%=)
+int a=10;
+System.out.println(a-=1);
+
+}
 }
